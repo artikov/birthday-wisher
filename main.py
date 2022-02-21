b0e642f503662330efc700dfe84e749c699cb4e9
Artikov <skrillem11@gmai.com>
2022-02-21 17:09:14 +0500

    final touch

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import pandas
 import datetime as dt
 import smtplib
 import random
+from private import *
+
+my_email = MY_EMAIL
+password = PASSWORD
 
 # -------------------- Get Today's dates ----------------------- #
 currentDay = dt.datetime.now().day
@@ -10,19 +14,34 @@ currentYear = dt.datetime.now().year
 
 
 # -------------------- Get dates from database ----------------------- #
-
-# 1. Update the birthdays.csv
 data = pandas.read_csv('birthdays.csv')
 data_dict = data.to_dict(orient='records')
 
-print(data_dict)
 
-# 2. Check if today matches a birthday in the birthdays.csv
+# -------------------- Function to send email ----------------------- #
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 
-# 4. Send the letter generated in step 3 to that person's email address.
+def send_email(letter_to_send):
+    with smtplib.SMTP_SSL(host='smtp.gmail.com') as connection:
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=RECIPIENT,
+            msg=f"Subject:Happy Birthday Dear!!!\n\n{letter_to_send}"
+        )
 
+
+# -------------------- Check if today someone's birthday ----------------------- #
+
+for n in data_dict:
+    if n['day'] == currentDay and n['month'] == currentMonth:
+        age = currentYear - n['year']
+        # if birthday, pick one letter and replace placeholders
+        random_letter = f'letter_templates/letter_{random.randint(1, 3)}.txt'
+        with open(f'{random_letter}') as letter_data:
+            letter = letter_data.read()
+            letter = letter.replace('[NAME]', f'{n["name"]}').replace('[AGE]', f'{age}')
+            send_email(letter)
 
 
 
